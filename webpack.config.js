4e518944344ea1b01a8bc373de1c37bc47f0274a
Alvin Hart <pageking123@gmail.com>
2022-10-19 08:24:31 +0900

    Finish recipes

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
-      filename: 'index.html',
-    })
-  ]
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     templateContent: ({ htmlWebpackPlugin }) =>
+  //       '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
+  //       htmlWebpackPlugin.options.title +
+  //       '</title></head><body><div id="app"></div></body></html>',
+  //     filename: "index.html",
+  //   }),
+  // ],
 };
 
 module.exports = config;
